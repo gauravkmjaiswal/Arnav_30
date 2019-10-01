fe8182c4e21b4106a82fdd8775ab33c2e878bbd4
gauravkmjaiswal <56041706+gauravkmjaiswal@users.noreply.github.com>
2019-10-01 22:44:55 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,10 +1,10 @@
 e=0
 while True:
          a=input('Enteer the string')
-         if len(a)>=3 and a[0]==a[len(a)-1]:
+         if (len(a)>=3 and a[0]==a[len(a)-1]):
              e+=1
          b=input('Press N to terminate the program')
-         if b=='n' or b=='N':
+         if (b=='n' or b=='N'):
              break
 
-print('The Number of string with the given condition is',e)
+print('The Number of string with given condition is',e)
